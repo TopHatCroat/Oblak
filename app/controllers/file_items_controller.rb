932b4de492799024fa85a043b1d95c9e7f09b90e
TopHatCroat <martinovic.antonio@gmail.com>
2016-01-11 11:16:13 +0100

    something

--- app/controllers/file_items_controller.rb
+++ app/controllers/file_items_controller.rb
@@ -10,14 +10,47 @@ class FileItemsController < ApplicationController
   def create
     @file_item = FileItem.new(file_item_params)
 
-    if @file_item.save
-      format.html { redirect_to file_items_index_path, flash[:success] = "The file #{@file_item.name} has been uplaoded" }
-      format.json { head :no_content }
-    else
-      render "new"
+    respond_to do |format|
+      if @picture.save
+        format.html { redirect_to file_items_index_path, flash[:success] = "The file #{@file_item.name} has been uplaoded" }
+        format.json { render json: @file_item, status: :created, location: @file_item }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @file_item.errors, status: :unprocessable_entity }
+      end
     end
-
   end
+  #
+  #   #check if file is within picture_path
+  #   if params[:attachment]
+  #     #picture_path_params = params[:picture][:picture_path]
+  #     #create a new tempfile named fileupload
+  #     tempfile = Tempfile.new("fileupload")
+  #     tempfile.binmode
+  #     #get the file and decode it with base64 then write it to the tempfile
+  #     tempfile.write(Base64.decode64(params["attachemnt"]))
+  #
+  #     #create a new uploaded file
+  #     uploaded_file = ActionDispatch::Http::UploadedFile.new(:tempfile => tempfile, :filename => picture_path_params["filename"], :original_filename => picture_path_params["original_filename"])
+  #
+  #     #replace picture_path with the new uploaded file
+  #     params[:file_item] =  uploaded_file
+  #
+  #   end
+  #
+  #   @picture = Picture.new(params[:picture])
+  #
+  #   respond_to do |format|
+  #     if @picture.save
+  #       format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+  #       format.json { render json: @picture, status: :created, location: @picture }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @picture.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
 
   def destroy
     @file_item = FileItem.find(params[:id])
